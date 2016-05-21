110fe8fb6a0c8b4337657c912dd188bd59e0524c
kevineo <kevin.eugene.ong@gmail.com>
2016-05-21 19:47:29 +0800

    CRUD ready

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,51 @@
 class PicturesController < ApplicationController
+	before_action :find_picture, only: [:show, :edit, :update, :destroy]
 
 	def index
-	end
-
-	def new
+		@pictures = Picture.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		@picture = Picture.new(picture_params)
+
+		if @picture.save
+			redirect_to @picture, notice: "Success!"
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @picture.update(picture_params)
+			redirect_to @picture, notice: "Congratulations. Picture was updated"
+		else
+			render 'edit'
+		end
+	end
+
 	def edit
 	end
 
-	def delete
+	def destroy
+		@picture.destroy
+		redirect_to root_path
 	end
 
+	private
+
+	def picture_params
+		params.require(:picture).permit(:title, :description)
+	end
+
+	def find_picture
+		@picture = Picture.find(params[:id])
+	end
 
 end
